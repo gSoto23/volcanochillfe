3e784cfe31810eaa36fe888cd98524ab8159c607
Gerardo Soto <eddyngerardo@gmail.com>
2024-03-09 11:05:07 -0600

    Screenshot Fix

--- js/checkin.js
+++ js/checkin.js
@@ -25,13 +25,13 @@ function showData() {
 
         checkIn = '<span class="heading-4-v2">Hello ' + checkInData[0].guest + '</span><p>Thank you for booking our property. Below you will find a table with the necessary codes to access.<div id="dataPrint"></p><h4 class="vct">Volcanochill Codes</h4><section class="section section-lg bg-default text-center"><div class="container"><div class="table-custom-responsive"><table class="table-custom table-custom-striped table-custom-primary"><thead><tr><th>' +
             checkInData[0].check_in + '</th><th>' + checkInData[0].check_out + '</th></tr></thead><tbody><tr><td>Main Door</td><td>' + checkInData[0].main_door + '</td></tr><tr><td>Cabin #1</td><td>' + checkInData[0].cab_one + '</td></tr><tr><td>Cabin #2</td><td>' + checkInData[0].cab_two + '</td></tr><tr><td>Cabin #3</td><td>' + checkInData[0].cab_three + '</td></tr>' +
-            '</tbody></table></div></div></section><h6>Wifi: volcanochillCR - Pass: poasvolcano</h6></div><div class="form-wrap form-wrap-button"><button class="button button-lg button-primary button-full-width" id="saveData">Download Codes</button></div>';
+            '</tbody></table></div></div></section><h5>Wifi: volcanochillCR <br> Pass: poasvolcano</h5></div><div class="form-wrap form-wrap-button"><button class="button button-lg button-primary button-full-width" id="saveData">Download Codes</button></div>';
     }
     else {
 
         checkIn = '<span class="heading-4-v2">Hola ' + checkInData[0].guest + '</span><p>Gracias por reservar nuestra propiedad, a cotinuacion encontrara una tabla con los codigos necesarios para hacer su ingreso.<div id="dataPrint"></p><h4 class="vct">Volcanochill Codes</h4><section class="section section-lg bg-default text-center"><div class="container"><div class="table-custom-responsive"><table class="table-custom table-custom-striped table-custom-primary"><thead><tr><th>' +
             checkInData[0].check_in + '</th><th>' + checkInData[0].check_out + '</th></tr></thead><tbody><tr><td>Main Door</td><td>' + checkInData[0].main_door + '</td></tr><tr><td>Cabin #1</td><td>' + checkInData[0].cab_one + '</td></tr><tr><td>Cabin #2</td><td>' + checkInData[0].cab_two + '</td></tr><tr><td>Cabin #3</td><td>' + checkInData[0].cab_three + '</td></tr>' +
-            '</tbody></table></div></div></section><h6>Wifi: volcanochillCR - Pass: poasvolcano</h6></div><div class="form-wrap form-wrap-button"><button class="button button-lg button-primary button-full-width" id="saveData">Download Codes</button></div>';
+            '</tbody></table></div></div></section><h5>Wifi: volcanochillCR <br> Pass: poasvolcano</h5></div><div class="form-wrap form-wrap-button"><button class="button button-lg button-primary button-full-width" id="saveData">Download Codes</button></div>';
     }
 
     //Revisar el Pass para mostrar las contrasenas
@@ -43,14 +43,24 @@ function showData() {
             $("#check-in-code").css("display", "none");
             document.getElementById('check-in-user').innerHTML = checkIn;
             $("#saveData").on('click', function () {
+
                 // saveDiv('dataPrint', 'volcanochill checkin');
-                html2canvas(document.querySelector('#dataPrint'), {
-                    onrendered: function (canvas) {
-                        return Canvas2Image.saveAsPNG(canvas,null,null,'volcanochill');
-                    }
+                document.getElementById('dataPrint').scrollIntoView({
+                    behavior: 'smooth', // Para un desplazamiento suave
+                    block: 'end' // Para asegurarse de que el elemento esté en la parte inferior de la ventana
                 });
+
+                // Esperar hasta que se complete el desplazamiento
+                setTimeout(function () {
+                    html2canvas(document.querySelector('#dataPrint'), {
+                        onrendered: function (canvas) {
+                            return Canvas2Image.saveAsJPEG(canvas, null, null, 'volcanochill');
+                        }
+                    });
+                }, 1000); // Espera 1 segundo (puedes ajustar el tiempo según sea necesario)
             });
         }
+
         else {
             $("#reservation-code").css("border", "solid 2px red");
             $("#errorMsg").text("Use a valid reservation code.")
