f0795d157d8a47b42655c50bb58f1cdde4350f4c
Gerardo Soto <eddyngerardo@gmail.com>
2024-03-15 14:25:14 -0600

    Add Spanish veriosn

--- js/checkin.js
+++ js/checkin.js
@@ -23,14 +23,14 @@ function showData() {
 
     if (lenguage == "en") {
 
-        checkIn = '<span class="heading-4-v2">Hello ' + checkInData[0].guest + '</span><p>Thank you for booking our property. Below you will find a table with the necessary codes to access.<div id="dataPrint"></p><h4 class="vct">Volcanochill Codes</h4><section class="section section-lg bg-default text-center"><div class="container"><div class="table-custom-responsive"><table class="table-custom table-custom-striped table-custom-primary"><thead><tr><th>' +
-            checkInData[0].check_in + '</th><th>' + checkInData[0].check_out + '</th></tr></thead><tbody><tr><td>Main Door</td><td>' + checkInData[0].main_door + '</td></tr><tr><td>Cabin #1</td><td>' + checkInData[0].cab_one + '</td></tr><tr><td>Cabin #2</td><td>' + checkInData[0].cab_two + '</td></tr><tr><td>Cabin #3</td><td>' + checkInData[0].cab_three + '</td></tr>' +
+        checkIn = '<span class="heading-4-v2">Hello ' + checkInData[0].guest + '</span><p>Thank you for booking our property. Below you will find a table with the necessary codes to access.<div id="dataPrint"></p><h4 class="vct">Volcanochill Codes</h4><section class="section section-lg bg-default text-center"><div class="container"><div class="table-custom-responsive"><table class="table-custom table-custom-striped table-custom-primary"><thead><tr><th><span class="l-header">Check In</span>' +
+            checkInData[0].check_in + '</th><th><span class="l-header">Check Out</span>' + checkInData[0].check_out + '</th></tr></thead><tbody><tr><td>Main Door</td><td>' + checkInData[0].main_door + '</td></tr><tr><td>Cabin #1</td><td>' + checkInData[0].cab_one + '</td></tr><tr><td>Cabin #2</td><td>' + checkInData[0].cab_two + '</td></tr><tr><td>Cabin #3</td><td>' + checkInData[0].cab_three + '</td></tr>' +
             '</tbody></table></div></div></section><h5>Wifi: volcanochillCR <br> Pass: poasvolcano</h5></div><div class="form-wrap form-wrap-button"><button class="button button-lg button-primary button-full-width" id="saveData">Download Codes</button></div>';
     }
     else {
 
-        checkIn = '<span class="heading-4-v2">Hola ' + checkInData[0].guest + '</span><p>Gracias por reservar nuestra propiedad, a cotinuacion encontrara una tabla con los codigos necesarios para hacer su ingreso.<div id="dataPrint"></p><h4 class="vct">Volcanochill Codes</h4><section class="section section-lg bg-default text-center"><div class="container"><div class="table-custom-responsive"><table class="table-custom table-custom-striped table-custom-primary"><thead><tr><th>' +
-            checkInData[0].check_in + '</th><th>' + checkInData[0].check_out + '</th></tr></thead><tbody><tr><td>Main Door</td><td>' + checkInData[0].main_door + '</td></tr><tr><td>Cabin #1</td><td>' + checkInData[0].cab_one + '</td></tr><tr><td>Cabin #2</td><td>' + checkInData[0].cab_two + '</td></tr><tr><td>Cabin #3</td><td>' + checkInData[0].cab_three + '</td></tr>' +
+        checkIn = '<span class="heading-4-v2">Hola ' + checkInData[0].guest + '</span><p>Gracias por reservar nuestra propiedad, a cotinuacion encontrara una tabla con los codigos necesarios para hacer su ingreso.<div id="dataPrint"></p><h4 class="vct">Codigos Volcanochill</h4><section class="section section-lg bg-default text-center"><div class="container"><div class="table-custom-responsive"><table class="table-custom table-custom-striped table-custom-primary"><thead><tr><th><span class="l-header">Check In</span>' +
+            checkInData[0].check_in + '</th><th><span class="l-header">Check Out</span>' + checkInData[0].check_out + '</th></tr></thead><tbody><tr><td>Principal</td><td>' + checkInData[0].main_door + '</td></tr><tr><td>Cabaña #1</td><td>' + checkInData[0].cab_one + '</td></tr><tr><td>Cabaña #2</td><td>' + checkInData[0].cab_two + '</td></tr><tr><td>Cabaña #3</td><td>' + checkInData[0].cab_three + '</td></tr>' +
             '</tbody></table></div></div></section><h5>Wifi: volcanochillCR <br> Pass: poasvolcano</h5></div><div class="form-wrap form-wrap-button"><button class="button button-lg button-primary button-full-width" id="saveData">Download Codes</button></div>';
     }
 
@@ -63,7 +63,11 @@ function showData() {
 
         else {
             $("#reservation-code").css("border", "solid 2px red");
-            $("#errorMsg").text("Use a valid reservation code.")
+            if (lenguage == "en"){
+                $("#errorMsg").text("Use a valid reservation code.")
+            } else {
+                $("#errorMsg").text("Usa un codigo valido.")
+            };
         };
     });
 
